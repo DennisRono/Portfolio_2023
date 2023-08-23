1b6919ce939a5518b0600d015cd01bc84a205a65
DennisRono <dennisrkibet@gmail.com>
2023-08-23 11:04:36 +0300

    starter configs and routing

--- client/src/Routes.tsx
+++ client/src/Routes.tsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom'
+import Home from './routes/Home'
+import ErrorBoundary from './routes/ErrorBoundary'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<Home />} errorElement={<ErrorBoundary />} />
+    </>
+  )
+)
 
 const Routes = () => {
-  return <div>Routes</div>
+  return (
+    <Fragment>
+      <RouterProvider router={router} />
+    </Fragment>
+  )
 }
 
 export default Routes
---
+++ client/src/routes/NotFound.tsx
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react'
+
+const NotFound = () => {
+  return (
+    <Fragment>
+      <div className="notfound"></div>
+    </Fragment>
+  )
+}
+
+export default NotFound
